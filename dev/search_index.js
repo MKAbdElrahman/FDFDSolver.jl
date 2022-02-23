e79ac158649ec4ecbc35afe3122e6cd79c6b1b8b
Documenter.jl <documenter@juliadocs.github.io>
2022-02-23 07:57:59 +0000

    build based on 566d596

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = FDFDSolver","category":"page"},{"location":"#FDFDSolver","page":"Home","title":"FDFDSolver","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for FDFDSolver.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [FDFDSolver]","category":"page"}]
+}
